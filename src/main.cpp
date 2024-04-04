2bd0decebccb460361e6c65259429738c6ca62d6
Oliver Stotzem <5693011+stotzem@users.noreply.github.com>
2024-04-04 08:01:40 +0200

    Refactor to Main class

--- src/main.cpp
+++ src/main.cpp
@@ -1,44 +1,74 @@
 #include "SFML/Graphics.hpp"
 
-int main() {
-    sf::RenderWindow window = sf::RenderWindow();
-    window.create(sf::VideoMode(1280, 800), "Game");
-    window.setVerticalSyncEnabled(true);
+class Main {
+  public:
 
-    std::vector<sf::CircleShape> shapes;
-    shapes.emplace_back(20).setFillColor(sf::Color::Red);
-    shapes.emplace_back(20).setFillColor(sf::Color::Blue);
-    shapes.emplace_back(20).setFillColor(sf::Color::Green);
+    Main() {
+        window.create(sf::VideoMode(1280, 800), "Game");
+        window.setVerticalSyncEnabled(true);
+        fieldLength = getFieldLength();
+        dotSize = fieldLength / (float) rows;
 
-    std::map<std::pair<int, int>,  int> field;
-    const int rows = 8;
-    const int cols = 8;
-    for (int y = 0; y < rows; ++y) {
-        for (int x = 0; x < cols; ++x) {
-            field[std::make_pair(x, y)] = rand() % 3 + 1;
+        for (int y = 0; y < rows; ++y) {
+            for (int x = 0; x < rows; ++x) {
+                field[std::make_pair(x, y)] = rand() % 3 + 1;
+            }
+        }
+
+        shapes.emplace_back(dotSize / 2 - 4).setFillColor(sf::Color::Red);
+        shapes.emplace_back(dotSize / 2 - 4).setFillColor(sf::Color::Blue);
+        shapes.emplace_back(dotSize / 2 - 4).setFillColor(sf::Color::Green);
+    }
+
+    void run() {
+        while (window.isOpen()) {
+            sf::Event event{};
+            while (window.pollEvent(event)) {
+                switch (event.type) {
+                case sf::Event::Closed:
+                    window.close();
+                    break;
+                default:
+                    break;
+                }
+            }
+            window.clear();
+            drawField();
+            window.display();
         }
     }
 
-    while (window.isOpen()) {
-        sf::Event event{};
-        while (window.pollEvent(event)) {
-            switch (event.type) {
-            case sf::Event::Closed:
-                window.close();
-                break;
-            default:
-                break;
-            }
-        }
-        window.clear(sf::Color(255,255,255,255));
+  private:
+
+    void drawField() {
         for (const auto& pair : field) {
             int x = pair.first.first;
             int y = pair.first.second;
             int pieceType = pair.second - 1;
             sf::CircleShape& shape = shapes[pieceType];
-            shape.setPosition((float) x * 44 + 20, (float) y * 44 + 20);
+            shape.setPosition((float) x * dotSize + 20, (float) y * dotSize + 20);
             window.draw(shape);
         }
-        window.display();
     }
+
+    float getFieldLength() {
+        if (window.getSize().x < window.getSize().y) {
+            return (float) window.getSize().x - 40;
+        }
+        else {
+            return (float) window.getSize().y - 40;
+        }
+    }
+
+    sf::RenderWindow window = sf::RenderWindow();
+    std::vector<sf::CircleShape> shapes;
+    const int rows = 8;
+    float fieldLength;
+    float dotSize;
+    std::map<std::pair<int, int>,  int> field;
+};
+
+int main() {
+    Main main;
+    main.run();
 }
